e430fe3b0d4a0f3372457c4390f220a05f563ac4
Stefan Gordon <stefan@stefangordon.com>
2017-04-27 10:26:07 -0700

    Initial prototype

---
+++ IdlePrevention/Native.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdlePrevention
+{
+    public enum EXECUTION_STATE : uint
+    {
+        ES_AWAYMODE_REQUIRED = 0x00000040,
+        ES_CONTINUOUS = 0x80000000,
+        ES_DISPLAY_REQUIRED = 0x00000002,
+    }
+
+    internal class Native
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+    }
+}
--- IdlePrevention/Program.cs
+++ IdlePrevention/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IdlePrevention.Properties;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,68 @@ namespace IdlePrevention
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new IdleContext());
+        }
+    }
+
+    public class IdleContext : ApplicationContext
+    {
+        private bool enabled;
+        private NotifyIcon trayIcon;
+        Timer idleTimer;
+
+        public IdleContext()
+        {
+            // Disable at start
+            enabled = false;
+
+            // Configure timer for 50 seconds, as the shortest windows screen idle time is 60 seconds.
+            idleTimer = new Timer();
+            idleTimer.Interval = 50 * 1000;
+            idleTimer.Tick += TimerElapsed;
+
+            // Setup the menu
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resources.AppIcon,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Enable Idle Prevention", ToggleIdle),
+                new MenuItem("Quit Program", Quit)
+            }),
+                Visible = true
+            };
+        }
+
+        private void TimerElapsed(object sender, EventArgs e)
+        {
+            Native.SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED);
+        }
+
+        void ToggleIdle(object sender, EventArgs e)
+        {
+            MenuItem menu = (MenuItem)sender;
+
+            if (enabled)
+            { 
+                // Turn off
+                menu.Text = "Enable Idle Prevention";
+                idleTimer.Stop();
+            }
+            else
+            {
+                // Turn on
+                menu.Text = "Disable Idle Prevention";
+                idleTimer.Start();
+            }
+
+            enabled = !enabled;
+        }
+
+        void Quit(object sender, EventArgs e)
+        {
+            // Hide icon before exiting
+            trayIcon.Visible = false;
+            Application.Exit();
         }
     }
 }
